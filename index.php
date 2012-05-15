05a7886d939ae730175efce157adbc9f4b62f365
Codemis <johnathan@still-water.com>
2012-05-14 18:31:16 -0700

    Adding missing code.

--- index.php
+++ index.php
@@ -21,3 +21,73 @@
  * 
  */
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Missional Digerati:: Redeeming Technology for Kingdom Work</title>
+		<!--[if lt IE 9]>
+	    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+	  <![endif]-->
+			<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+			<script type="text/javascript" charset="utf-8" src="javascript/bootstrap.min.js"></script>
+			<script type="text/javascript" charset="utf-8" src="javascript/bootstrap-dropdown.min.js"></script>
+			<script type="text/javascript" charset="utf-8" src="javascript/application.js"></script>
+			<link rel="stylesheet" href="stylesheets/bootstrap.min.css" type="text/css" charset="utf-8">
+			<link rel="stylesheet" href="stylesheets/bootstrap-responsive.min.css" type="text/css" charset="utf-8">
+			<link rel="stylesheet" href="stylesheets/application.css" type="text/css" charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<div class="navbar navbar-fixed-top">
+	    <div class="navbar-inner">
+	      <div class="container">
+	        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	          <span class="icon-bar"></span>
+	        </a>
+	        <a class="brand" href="#"><img src="images/site/logo.png" alt="Missional Digerati" title="Missional Digerati" /></a>
+	        <div class="nav-collapse">
+						<ul class="nav pull-right">
+							<li><a href="/" title="Home">Home</a></li>
+							<li><a href="/" title="About Us">About Us</a></li>
+							<li class="dropdown" id="#projects">
+								<a href="#projects" title="Projects" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="/" title="Joshua Project Mobile App">Joshua Project Mobile App</a></li>
+									<li><a href="/" title="Open Bible Stories">Open Bible Stories</a></li>
+									<li><a href="/" title="Seven Nine Online">Seven Nine Online</a></li>
+								</ul>
+							</li>
+							<li><a href="/" title="Contact Us">Contact Us</a></li>
+							<li class="donate"><div><a class="btn btn-primary" href="/donate" title="Donate">Donate</a></div></li>
+						</ul>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+		<div class="slider">
+			<div class="inner">
+				<div class="container">
+				</div>
+			</div>
+		</div>
+	  <div class="content">
+			<div class="inner">
+				<div class="container">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</div>
+		</div>
+		<div class="extra">
+			<div class="inner">
+				<div class="container">
+					<div class="row">
+					  <div class="span10 offset1 mission">
+							<p><span class="highlighted">Missional Digerati</span>, a community of thinkers, designers and developers committed to exploiting breakthroughs in technology in order to advance the pace of reaching every person on earth with the good news of Jesus Christ.</p>
+						</div>
+						<div class="span1"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>

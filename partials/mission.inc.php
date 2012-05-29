c07a9c837b76742b00956358a96aea91cd0cf888
Codemis <johnathan@still-water.com>
2012-05-29 12:34:40 -0700

    Added GPL license.

--- partials/mission.inc.php
+++ partials/mission.inc.php
@@ -1,3 +1,26 @@
+<?php
+/**
+ * This file is part of Missional Digerati Website.
+ * 
+ * Missional Digerati Website is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Missional Digerati Website is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @copyright Copyright 2012 Missional Digerati
+ * 
+ */
+?>
 <div class="row">
   <div class="span10 offset1 mission">
 		<p><span class="highlighted">Missional Digerati</span>, a community of thinkers, designers and developers committed to exploiting breakthroughs in technology in order to advance the pace of reaching every person on earth with the good news of Jesus Christ.</p>

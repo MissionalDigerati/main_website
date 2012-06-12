e964a91a933e9ffa56b506b445b9973a6df0e4de
Codemis <johnathan@still-water.com>
2012-06-12 12:29:28 -0700

    Added tagline to site.

--- partials/main_nav.inc.php
+++ partials/main_nav.inc.php
@@ -24,7 +24,10 @@
 <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 <span class="icon-bar"></span>
 </a>
-<a class="brand" href="#"><img src="/images/site/logo.png" alt="Missional Digerati" title="Missional Digerati" id="logo" /></a>
+<a class="brand" href="/">
+	<img src="/images/site/logo.png" alt="Missional Digerati" title="Missional Digerati" id="logo" /><br>
+	<span class="tagline">Mobile & Web Apps for the Christian Missional Community</span>
+</a>
 <div class="nav-collapse">
 	<ul class="nav pull-right">
 		<li><a href="/" title="Home">Home</a></li>
@@ -33,13 +36,13 @@
 			<a href="#projects" title="Projects" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
 			<ul class="dropdown-menu">
 				<li><a href="/projects/jp-mobile" title="Joshua Project Mobile App">Joshua Project Mobile App</a></li>
-				<li><a href="/" title="Open Bible Stories">Open Bible Stories</a></li>
+				<li><a href="/projects/open-bible-stories" title="Open Bible Stories">Open Bible Stories</a></li>
 				<li><a href="/projects/79-online" title="Seven Nine Online">Seven Nine Online</a></li>
 			</ul>
 		</li>
 		<li><a href="/" title="Contact Us">Submit an Idea</a></li>
 		<li><a href="/" title="Contact Us">Contact Us</a></li>
 		<!-- <li class="visible-phone"><a href="/" title="Donate">Donate</a></li>
-									<li class="donate hidden-phone"><div><a class="btn btn-primary" href="/donate" title="Donate">Donate</a></div></li> -->
+				<li class="donate hidden-phone"><div><a class="btn btn-primary" href="/donate" title="Donate">Donate</a></div></li> -->
 	</ul>
 </div>

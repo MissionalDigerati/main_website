b8e1734e8a2dfe8f76792d9514dc9bbaec6eb574
Codemis <johnathan@missionaldigerati.com>
2012-07-07 12:32:57 -0500

    Updated mission statement to use wording from Victor Dorado.

--- partials/mission.inc.php
+++ partials/mission.inc.php
@@ -23,7 +23,7 @@
 ?>
 <div class="row">
   <div class="span10 offset1 mission">
-		<p><span class="highlighted">Missional Digerati</span>, a community of thinkers, designers and developers committed to exploiting breakthroughs in technology in order to advance the pace of reaching every person on earth with the good news of Jesus Christ.</p>
+		<p><span class="highlighted">Missional Digerati</span> is a  community of thinkers, designers and developers committed to leveraging technology to advance the pace of reaching every person on earth with the good news of Jesus Christ.</p>
 	</div>
 	<div class="span1"></div>
 </div>
